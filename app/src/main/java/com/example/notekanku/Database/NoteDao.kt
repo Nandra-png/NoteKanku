7b259d0d8d34204d7a34084d636fa050dbbb42f8
Nandra-png <rujakapel28@gmail.com>
2023-02-16 21:12:08 +0700

    udah

---
+++ app/src/main/java/com/example/notekanku/Database/NoteDao.kt
@@ -0,0 +1,24 @@
+package com.example.notekanku.Database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.notekanku.model.Note
+
+
+@Dao
+interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(note : Note)
+
+    @Delete
+    suspend fun delete(note : Note)
+
+    @Query("Select * from notes_table order by id ASC")
+    fun getAllNotes() : LiveData<List<Note>>
+
+
+    @Query("UPDATE notes_table Set title = :title, note = :note WHERE id = :id")
+    suspend fun update(id : Int?, title : String?, note : String?)
+
+}
---
+++ app/src/main/java/com/example/notekanku/utilities/constants.kt
@@ -0,0 +1,3 @@
+package com.example.notekanku.utilities
+
+const val DATABASE_NAME = "note_database"
